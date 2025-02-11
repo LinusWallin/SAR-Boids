9519aff3e5e44d427072818d4ee27cf95bea1a25
Linus Wallin <linuswallin@live.se>
2025-02-11 14:17:46 +0100

    feat: adds settings file for boids

---
+++ Assets/Scipts/BoidSettings.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "BoidSettings", menuName = "Scriptable Objects/BoidSettings")]
+public class BoidSettings : ScriptableObject
+{
+    public int numBoids;
+    public float boidRadius;
+    
+    public float minSpeed;
+    public float maxSpeed;
+    public float maxSteerForce;
+    
+    public float separationWeight;
+    public float alignmentWeight;
+    public float cohesionWeight;
+
+}
